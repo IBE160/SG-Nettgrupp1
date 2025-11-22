110e1c45f33b346c9754902641287f9d19e1d82d
elyder <lyder@kleppnett.no>
2025-11-22 19:23:34 +0100

    resolving merge conflicts

--- api-server.js
+++ api-server.js
@@ -4,14 +4,16 @@ import { supabase } from './api/config/supabase.js'; // Import supabase client
 
 
 import productRoutes from './api/routes/products.js';
+import cartRoutes from './api/routes/cart.js';
 
 const app = express();
 
 // Må komme FØR routerne, ellers er req.body = undefined
 app.use(express.json());
 
-// Mount the products router
+// Mount routers
 app.use('/api/products', productRoutes);
+app.use('/api/cart', cartRoutes);
 
 const PORT = 3001;
 app.listen(PORT, () => {
