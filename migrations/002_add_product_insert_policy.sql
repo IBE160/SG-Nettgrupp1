fae210a39aad06cfeb7c6a6d26998fbbc04ad29b
cahag94 <cahag4982@himolde.no>
2025-12-08 18:44:14 +0100

    remove unused migration files and AdminOrderDetailPage component

--- migrations/002_add_product_insert_policy.sql
+++
@@ -1,7 +0,0 @@
--- Migration: Add an INSERT policy for the products table
--- This allows anonymous users to create products, which is necessary for the API tests to pass.
-
-CREATE POLICY "Anon users can create products"
-ON products FOR INSERT
-TO anon
-WITH CHECK (true);
